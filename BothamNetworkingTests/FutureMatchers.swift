8f4214dedd1922d3caaea93827e6615d44bafe86
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-12-30 15:32:07 +0100

    Move BothamAPIClientError equatable implementation to the tests target

--- BothamNetworkingTests/FutureMatchers.swift
+++ BothamNetworkingTests/FutureMatchers.swift
@@ -50,3 +50,20 @@ func failWithError<T>(expectedError: NSError) -> MatcherFunc<T?> {
         }
     }
 }
+
+extension BothamAPIClientError: Equatable { }
+
+public func == (lhs: BothamAPIClientError, rhs: BothamAPIClientError) -> Bool {
+    switch (lhs, rhs) {
+    case let (.HTTPResponseError(statusCode1, body1), .HTTPResponseError(statusCode2, body2)):
+        return statusCode1 == statusCode2 && body1 == body2
+    case let (.HTTPClientError(error1), .HTTPClientError(error2)):
+        return error1 == error2
+    case (.NetworkError, .NetworkError):
+        return true
+    case (.UnsupportedURLScheme, .UnsupportedURLScheme):
+        return true
+    default:
+        return false
+    }
+}
