d6efb2591e9a8dc84c8ff26c02a2d22d2523556f
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-12-30 11:01:04 +0100

    Fix swiftlint violation

--- BothamNetworking/Dictionary.swift
+++ BothamNetworking/Dictionary.swift
@@ -12,7 +12,7 @@ extension Dictionary {
     init<S: SequenceType where S.Generator.Element == Element>
         (_ seq: S) {
             self.init()
-            for (k,v) in seq {
+            for (k, v) in seq {
                 self[k] = v
             }
     }
