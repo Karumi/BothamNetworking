3ba1b0868e38242fe5ce13a4c5ef70400282eed4
Davide Mendolia <davide@gokarumi.com>
2016-01-01 22:10:11 +0100

    Fix spacing

--- BothamNetworking/NSHTTPClient.swift
+++ BothamNetworking/NSHTTPClient.swift
@@ -49,6 +49,6 @@ class NSHTTPClient: HTTPClient {
         let headers = response.allHeaderFields.map {
             (key, value) in (key as! String, value as! String)
         }
-            return HTTPResponse(statusCode: statusCode, headers: CaseInsensitiveDictionary(dictionary: Dictionary(headers)), body: data)
+        return HTTPResponse(statusCode: statusCode, headers: CaseInsensitiveDictionary(dictionary: Dictionary(headers)), body: data)
     }
 }
